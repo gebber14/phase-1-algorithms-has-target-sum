688676444ff9ba54e0d898549ed1ed58a69b5f38
Jason Rothgeb <jrothgeb@gmail.com>
2022-08-04 17:19:10 -0500

    commit tree a4b36f1cea1eea43db25755a51a920ad5893eae4

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  for (let i=0; i < array.length; i++) {
+    const lastElement = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+    if ((array[j]) === lastElement) {
+      return true;
+    }
+   } 
+  }
+  return false;
 }
 
 /* 
